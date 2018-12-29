368a33d1f45ac78a879fd602c3b94d47a44ab4fa
Pavel Lavrenov <lavrenov.pavel@gmail.com>
2018-12-29 04:27:03 +0300

    upd

--- index.php
+++ index.php
@@ -4,28 +4,36 @@ require_once 'variables.php';
 define('DECODE_FILE_PATH', './decode_file.php');
 define('ENCODE_FILE_PATH', './encode_file.php');
 
+define('REPEATER_COUNT', 2);
+
 define('GLOBAL_VARIABLES', [
-    'название_массива',
-    'название_второго_массива'
+    '____962051083',
 ]);
 
 define('GLOBAL_FUNCTIONS', [
-    'название_функции'
+    '___652086434'
 ]);
 
 class Decode {
 
     private $file;
 
+    public $console_output = '';
+
     function __construct() {
         $this->init();
+    }
+
+    private function console_log($str)
+    {
+        $this->console_output .= $str . "\n";
     }
 
     public function init()
     {
         !defined('DECODE_FILE_PATH') ? die('Не задана константа "DECODE_FILE_PATH"') : null;
 
-        !file_exists(constant('DECODE_FILE_PATH')) ? die('Файл "DECODE_FILE_PATH" не найден') : null;
+        !file_exists( DECODE_FILE_PATH ) ? die('Файл "DECODE_FILE_PATH" не найден') : null;
 
         $this->file = file_get_contents( DECODE_FILE_PATH );
 
@@ -35,7 +43,7 @@ class Decode {
 
         $this->prepare();
 
-        file_put_contents(constant('ENCODE_FILE_PATH'), $this->file);
+        file_put_contents( ENCODE_FILE_PATH , $this->file);
     }
 
     private function edit_variables_array($const_name)
@@ -54,7 +62,7 @@ class Decode {
         if (defined($const_name)) {
             foreach (constant($const_name) as $key => $value) {
                 $this->file = preg_replace_callback('/'.$value.'\((\d+)\)/', function($matches) use ($value) {
-                    return "'".$value($matches[1])."'";
+                    return "'" . $value($matches[1]) . "'";
                 }, $this->file);
             }
         }
@@ -62,15 +70,33 @@ class Decode {
 
     private function prepare()
     {
-        $this->prepare_round();
+        for ($i = 0; $i < REPEATER_COUNT; $i++) {
+            $this->prepare_func();
+        }
 
         $this->prepare_compute();
     }
 
-    private function prepare_round()
+    private function prepare_func()
     {
-        $this->file = preg_replace_callback('/round\((.+?)\)/', function($matches) {
-            return round($matches[1]);
+        $this->file = preg_replace_callback('/(min|round|strtoupper|strrev)\([^\(\)\$]+\)/', function($matches) {
+            $result = eval("return $matches[0];");
+
+            $this->console_log(gettype($result) . ' - ' . $matches[0] . ' --- ' . eval("return $matches[0];"));
+
+            switch (gettype($result)) {
+                case 'string':
+                    return "'" . $result . "'";
+                    break;
+                case 'double':
+                    return $result;
+                    break;
+                case 'integer':
+                    return $result;
+                    break;
+                default:
+                    break;
+            }
         }, $this->file);
     }
 
@@ -81,11 +107,13 @@ class Decode {
         }, $this->file);
     }
 
-
 }
 
 $decode = new Decode;
 
 
+echo '<pre>';
+print_r($decode->console_output);
+echo '</pre>';
 
 
